74841c30884041f11bd62f5d2f8aacad866b54f7
Yersa Nordman <yersa@finwo.nl>
2023-03-14 23:38:10 +0100

    Use softprops' work instead of curling ourselves

--- scripts/gha-attach-assets.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-pwd
-
-      # - run: 'curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GITHUB_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" -H "Content-Type: application/octet-stream" https://uploads.github.com/repos/${GITHUB_REPOSITORY}/tags/${GITHUB_REF/refs\/tags\//}/assets?name=pmlag-linux-amd64.tar.gz --data-binary "@package/pmlag-linux-amd64.tar.gz"'
--- scripts/gha-create-release.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-pwd
-
-        # 'curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GITHUB_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/${GITHUB_REPOSITORY}/releases --data "{\\"tag_name\\":\\${GITHUB_REF/refs\/tags\//}"\\"}"'
